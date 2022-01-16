07d2f35e7f45ad643f4ae8a2018bb79024708177
mhecate <maral.serik.99@mail.ru>
2022-01-16 23:02:54 +0600

    final upgrade - byli ispravleny oshibki v rasschete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,13 @@ import java.util.stream.Collectors;
 
 
 public class Main {
-    private static final long BYTE_TO_MB_CONVERSION_VALUE = 1024 * 1024;
-
     public static void main(String[] args) throws IOException, ParseException {
         Scanner in = new Scanner(System.in);
         System.out.println("VVedite datu v formate  :  29-Mar-2020");
         String inputDate = in.next();
+
         List<Transaction> list = new ArrayList<>();
+
         try {
             FileReader fileReader = new FileReader("C:\\testovoezadanie\\Records.csv");
             CSVReader csvReader = new CSVReader(fileReader);
@@ -29,15 +29,13 @@ public class Main {
                     continue;
                 }
                 Transaction transaction = new Transaction();
-                String date = nextRecord[0];
-                if(inputDate.equalsIgnoreCase(date)) {
-                    transaction.setDate(date);
+                if (inputDate.equalsIgnoreCase(nextRecord[0])) {
+                    transaction.setDate(nextRecord[0]);
                     transaction.setDesc(nextRecord[1]);
                     transaction.setDeposits(Double.parseDouble(nextRecord[2].replaceAll(",", "")));
                     transaction.setWithdrawls(Double.parseDouble(nextRecord[3].replaceAll(",", "")));
                     transaction.setBalance(Double.parseDouble(nextRecord[4].replaceAll(",", "")));
                     list.add(transaction);
-                    //System.out.println(transaction.getDesc());
                 }
 
             }
@@ -45,43 +43,31 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(inputDate + "minimalnoe znachenie polya Witdrawls " + " min : " + getMin(list));
-        System.out.println(inputDate + "maximalnoe znachenie polya Witdrawls " + " max : " + getMax(list));
-        System.out.println(inputDate + "srednee znachenie polya Witdrawls " + " average : " + getAverage(list));
+
+        System.out.println(inputDate + " minimalnoe znachenie polya Witdrawls " + " min : " + getMin(list));
+        System.out.println(inputDate + " maximalnoe znachenie polya Witdrawls " + " max : " + getMax(list));
+        System.out.println(inputDate + " srednee znachenie polya Witdrawls " + " average : " + getAverage(list));
+
         List<Transaction> sortedList = list.stream()
                 .sorted((o1, o2) -> o1.getDesc().compareTo(o2.getDesc()))
                 .collect(Collectors.toList());
+
         String str = sortedList.get(0).getDesc();
-        int fromIndex = 0;
+
+        int fromIndex = 1;
         int toIndex = 0;
-        for(int i = 1; i<sortedList.size(); i++){
-            if(str.equals(sortedList.get(i).getDesc())){
+        for (int i = 0; i < sortedList.size(); i++) {
+            if (str.equals(sortedList.get(i).getDesc())) {
                 toIndex++;
-            }
-            else if(!str.equals(sortedList.get(i).getDesc())){
-                System.out.println(inputDate + " " + str + " min : " + getMin(sortedList.subList(fromIndex, toIndex)));
-                System.out.println(inputDate + " " + str + " max : " + getMax(sortedList.subList(fromIndex, toIndex)));
-                System.out.println(inputDate + " " + str + " average : " + getAverage(sortedList.subList(fromIndex, toIndex)));
-                //System.out.println(sortedList.get(i).getDesc());
+            } else if (!str.equals(sortedList.get(i).getDesc())) {
+                System.out.println(inputDate + " " + str + " min : " + getMin(sortedList.subList(fromIndex - 1, toIndex)));
+                System.out.println(inputDate + " " + str + " max : " + getMax(sortedList.subList(fromIndex - 1, toIndex)));
+                System.out.println(inputDate + " " + str + " average : " + getAverage(sortedList.subList(fromIndex - 1, toIndex)));
                 str = sortedList.get(i).getDesc();
-                fromIndex =toIndex;
+                fromIndex = toIndex;
             }
-
         }
-
-        long start = System.nanoTime();
-        double elapsedTimeInSec = (System.nanoTime() - start) * 1.0e-9;
-        System.out.println(elapsedTimeInSec + " seconds");
-
-        Runtime runtime = Runtime.getRuntime();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        System.out.println("Used Memory before: " + usedMemoryBefore);
-        // working code here
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        System.out.println("Memory increased: " + (usedMemoryAfter-usedMemoryBefore));
-;
     }
-
 
 
     private static String getMin(List<Transaction> list) {
@@ -89,9 +75,8 @@ public class Main {
                 .stream()
                 .min(Comparator.comparing(Transaction::getWithdrawls))
                 .orElseThrow(NoSuchElementException::new);
-       return minByWithDrawls.toString();
+        return minByWithDrawls.toString();
     }
-
 
 
     private static String getMax(List<Transaction> list) {
@@ -103,7 +88,6 @@ public class Main {
     }
 
 
-
     private static String getAverage(List<Transaction> list) {
 
         return String.valueOf(list
@@ -111,6 +95,4 @@ public class Main {
                 .mapToDouble(Transaction::getWithdrawls)
                 .average().orElse(Double.NaN));
     }
-
-    public static void forFun(){}
 }
